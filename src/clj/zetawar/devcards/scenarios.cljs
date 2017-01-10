16c8144dc06d70d60cf0feb792289bda09e38ef4
David Whittington <djwhitt@gmail.com>
2017-01-10 20:02:19 +0000

    Little bit of scenario devcards refactoring

--- src/clj/zetawar/devcards/scenarios.cljs
+++ src/clj/zetawar/devcards/scenarios.cljs
@@ -1,9 +1,7 @@
 (ns zetawar.devcards.scenarios
   (:require
-   [datascript.core :as d]
    [devcards.core :as dc :include-macros true]
    [integrant.core :as ig]
-   [posh.core :as posh]
    [reagent.core :as r]
    [zetawar.app :as app]
    [zetawar.system :as system]
@@ -11,16 +9,18 @@
   (:require-macros
    [devcards.core :refer [defcard defcard-rg]]))
 
-(defcard-rg sterlings-aruba-multiplayer-card
+(defn init-scenario [scenario-id]
   (let [system (ig/init system/game-config)
-        game-cfg (:zetawar.system/game system)
-        views-cfg (:zetawar.system/game-views system)]
-    (app/start-new-game! game-cfg :sterlings-aruba-multiplayer)
+        game-cfg (:zetawar.system/game system)]
+    (app/start-new-game! game-cfg scenario-id)
+    system))
+
+(defcard-rg sterlings-aruba-multiplayer-card
+  (let [views-cfg (-> (init-scenario :sterlings-aruba-multiplayer)
+                      :zetawar.system/game-views)]
     [views/board views-cfg]))
 
 (defcard-rg city-sprawl-multiplayer-card
-  (let [system (ig/init system/game-config)
-        game-cfg (:zetawar.system/game system)
-        views-cfg (:zetawar.system/game-views system)]
-    (app/start-new-game! game-cfg :city-sprawl-multiplayer)
+  (let [views-cfg (-> (init-scenario :city-sprawl-multiplayer)
+                      :zetawar.system/game-views)]
     [views/board views-cfg]))
