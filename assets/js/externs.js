58e054af40a02428079155c0141b24c00a61c4f3
David Whittington <djwhitt@gmail.com>
2017-02-14 20:51:07 +0000

    Fixing externs

--- assets/js/externs.js
+++ assets/js/externs.js
@@ -4,10 +4,11 @@ var lzwEncode = function(s) {};
 /** @return {string} */
 var lzwDecode = function(s) {};
 
-var ZetawarAI = function() {};
-ZetawarAI.makeActorContext = function(db, game actor) {};
-ZetawarAI.scoreActor = function(db, game actor, actor_ctx) {};
-ZetawarAI.makeBaseActionContext = function(db, game, actor_ctx, base) {};
-ZetawarAI.scoreBaseAction = function(db, game, actor_ctx, action) {};
-ZetawarAI.makeUnitActionContext = function(db, game, actor_ctx, unit) {};
-ZetawarAI.scoreUnitAction = function(db, game, action_ctx, action) {};
+var ZetawarAI = {
+    "makeActorContext": function() {},
+    "scoreActor": function() {},
+    "makeBaseActionContext": function() {},
+    "scoreBaseAction": function() {},
+    "makeUnitActionContext": function() {},
+    "scoreUnitAction": function() {},
+};
