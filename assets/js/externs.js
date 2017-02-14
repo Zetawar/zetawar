cf4ef94b3e076f156654f07f2780dc35d8110bf2
David Whittington <djwhitt@gmail.com>
2017-02-14 19:46:34 +0000

    Adding externs for custom JS AI

--- assets/js/externs.js
+++ assets/js/externs.js
@@ -3,3 +3,11 @@ var lzwEncode = function(s) {};
 
 /** @return {string} */
 var lzwDecode = function(s) {};
+
+var ZetawarAI = function() {};
+ZetawarAI.maxActorContext = function(db, game actor) {};
+ZetawarAI.scoreActor = function(db, game actor, actor_ctx) {};
+ZetawarAI.makeBaseActionContext = function(db, game, actor_ctx, base) {};
+ZetawarAI.scoreBaseAction = function(db, game, actor_ctx, action) {};
+ZetawarAI.makeUnitActionContext = function(db, game, actor_ctx, unit) {};
+ZetawarAI.scoreUnitAction = function(db, game, action_ctx, action) {};
