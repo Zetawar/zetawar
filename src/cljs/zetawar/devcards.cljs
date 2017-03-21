b2a8eb3e6e31957fc809dbd9e4c7b63fae93ad1b
David Whittington <djwhitt@gmail.com>
2017-03-21 16:29:32 -0500

    Rename specs devcard to game-specs

--- src/cljs/zetawar/devcards.cljs
+++ src/cljs/zetawar/devcards.cljs
@@ -2,10 +2,10 @@
   (:require
    [devcards.core :as devcards :include-macros true]
    [zetawar.devcards.data-formats]
+   [zetawar.devcards.game-specs]
    [zetawar.devcards.maps-and-scenarios]
    [zetawar.devcards.prototype]
    [zetawar.devcards.selection-and-target]
-   [zetawar.devcards.specs]
    [zetawar.site :as site]))
 
 (when (site/viewing-devcards?)
