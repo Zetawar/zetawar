6de41dcd41321dc798895ca527bf298dbc76009b
Tim Bedford <tmbdfd@gmail.com>
2017-06-27 19:03:23 -0400

    Add :free-attack-once state-map

--- src/cljs/zetawar/data.cljs
+++ src/cljs/zetawar/data.cljs
@@ -146,6 +146,23 @@
                              :capture-base :done}}
        :done               {}}}
 
+     :free-attack-once
+     {:start-state :start
+      :built-state :done
+      :states
+      {:start              {:transitions
+                            {:move-unit    :moved-1-attacked-0
+                             :attack-unit  :moved-0-attacked-1
+                             :repair-unit  :done
+                             :capture-base :done}}
+       :moved-0-attacked-1 {:transitions
+                            {:move-unit    :done
+                             :capture-base :done}}
+       :moved-1-attacked-0 {:transitions
+                            {:attack-unit  :done
+                             :capture-base :done}}
+       :done               {}}}
+
      :free-attack-twice
      {:start-state :start
       :built-state :done
