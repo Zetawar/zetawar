b72f58b62f8c16450b27a43e048c8bcea43b211a
Tim Bedford <tmbdfd@gmail.com>
2017-06-27 19:05:50 -0400

    Change order for :states in :move-attack-twice

--- src/cljs/zetawar/data.cljs
+++ src/cljs/zetawar/data.cljs
@@ -138,10 +138,10 @@
        :moved-1-attacked-0 {:transitions
                             {:attack-unit  :moved-1-attacked-1
                              :capture-base :done}}
-       :moved-1-attacked-1 {:transitions
+       :moved-0-attacked-1 {:transitions
                             {:attack-unit  :done
                              :capture-base :done}}
-       :moved-0-attacked-1 {:transitions
+       :moved-1-attacked-1 {:transitions
                             {:attack-unit  :done
                              :capture-base :done}}
        :done               {}}}
